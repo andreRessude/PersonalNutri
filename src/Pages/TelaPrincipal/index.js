0f02e30f274c1fc5a2da593fea4a91188a9b6a44
“andre” <anressude2006@gmail.com>
2023-09-14 00:47:33 -0300

    tabela nutricional

--- src/Pages/TelaPrincipal/index.js
+++ src/Pages/TelaPrincipal/index.js
@@ -1,9 +1,8 @@
 import React from "react"
-import { View, ScrollView } from 'react-native'
+import { View } from 'react-native'
 
 import styles from "./style"
 import BarraPesquisa from "../../Components/BarraPesquisa/barraPesquisa"
-import CardsPratos from "../../Components/CardsPratos/cardsPratos"
 import BotaoCamera from "../../Components/BotaoCamera/botaoCamera"
 
 export default function TelaPrincipal(){
